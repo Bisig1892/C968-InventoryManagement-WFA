6798c41151393fd34ce4844c6a9b83dd4532ada9
bisig1892 <bisig1892@gmail.com>
2024-06-23 21:40:24 -0700

    Added inhouse part class

--- Tyler Bisig - C968/InHouse.cs
+++ Tyler Bisig - C968/InHouse.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Tyler_Bisig___C968
 {
-    internal class InHouse : Part
+    public partial class InHouse : Part
     {
         private int machineId;
+        public int MachineId { get; set; }
+
+        public InHouse()
+        {
+
+        }
+
+        public InHouse(int partId, string name, decimal price, int inStock, int min, int max, int machineId)
+        {
+            PartId = partId;
+            Name = name;
+            Price = price;
+            InStock = inStock;
+            Min = min;
+            Max = max;
+            MachineId = machineId; 
+        }
     }
 }
