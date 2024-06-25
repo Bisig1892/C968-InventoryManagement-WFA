00163df2e39063378a11e7e70baa45f5f04069d3
bisig1892 <bisig1892@gmail.com>
2024-06-24 21:51:18 -0700

    Constructor created. add, remove, and lookup functions added for parts associated to product

--- Tyler Bisig - C968/Product.cs
+++ Tyler Bisig - C968/Product.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Tyler_Bisig___C968
 {
@@ -21,6 +22,52 @@ namespace Tyler_Bisig___C968
         public Product()
         {
         
+        }
+
+        public Product(int productId, string name, decimal price, int inStock, int min, int max)
+        {
+            ProductId = productId;
+            Name = name;
+            Price = price;
+            InStock = inStock;
+            Min = min;
+            Max = max;
+        }
+        // Adds part to product
+        public void addAssociatedPart(Part part) 
+        { 
+            AssociatedParts.Add(part);
+        }
+
+        // Removes part from product
+        public void removeAssociatedPart(int partId) 
+        {
+            foreach (Part part in AssociatedParts) 
+            {
+                if (part.PartId == partId)
+                {
+                    AssociatedParts.Remove(part);
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("Part not found.");
+                    return;
+                }
+            }
+            return;
+        }
+        // Finds parts associated with product
+        public Part lookupAssociatedPart(int partId)
+        {
+            foreach (Part part in AssociatedParts)
+            {
+                if (part.PartId == partId)
+                {
+                    return part;
+                }
+            }
+            return null;
         }
     }
 }
